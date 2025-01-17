31b666d8186bb58a0fd9d8dc459e3bf882f6c5d1
SKishchenko <userg@yandex.ru>
2025-01-17 21:59:05 +0300

    Update

--- bff-service/src/main/java/com/example/bff/controller/BffController.java
+++ bff-service/src/main/java/com/example/bff/controller/BffController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/bff")
+@RequestMapping("/api/site-bff")
 @RequiredArgsConstructor
 public class BffController {
     private final BffService bffService;
